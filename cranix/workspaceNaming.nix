8da12421ae670ab9b0884091a429332f6ae5a1c7
Emilio Ruscitti <emiliorccp@gmail.com>
2024-06-26 13:41:55 -0300

    fix: colision with `crane` workspace build script

--- cranix/workspaceNaming.nix
+++ cranix/workspaceNaming.nix
@@ -11,16 +11,10 @@ in
     isLibTarget ? false,
     ...
   } @ args: isDepsBuild: let
-    extraTargetFlag =
-      if args.isLibTarget or isLibTarget
-      then "lib"
-      else "bin";
     crateName = prev.crateNameFromCargoToml args;
-    cargoExtraArgWorkspace = "--${extraTargetFlag} ${args.workspaceTargetName}";
     depsFlag = optionalString isDepsBuild "-${crateName.pname}-deps";
   in
     optionalAttrs (builtins.typeOf (args.workspaceTargetName or workspaceTargetName) != "null") {
       pnameSuffix = depsFlag;
       pname = "${args.workspaceTargetName}";
-      cargoExtraArgs = "${args.cargoExtraArgs or ""}${cargoExtraArgWorkspace}";
     }
